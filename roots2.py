1cd6721a8973a37ee23c0e327ba25933dc148e48
Kenneth Gregory Larrieu <kglarrieu@ucdavis.edu>
2018-11-04 22:24:52 -0800

    color scheme

--- roots2.py
+++ roots2.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 import itertools as it
 import random as rd
 import time
@@ -41,26 +42,32 @@ def make_plot(coefs):
     x_roots = []
     y_roots = []
     all_degs = []
-    for i in range(1, 8):
+    max_deg = 7
+    for i in reversed(range(1, max_deg+1)):
         print(i)
         x, y, degs = get_roots(i, possible_coefficients=coefs)
         x_roots.extend(x)
         y_roots.extend(y)
         all_degs.extend(degs)
     print('making plot...')
-    plt.scatter(x_roots, y_roots, s=0.01, marker='.', edgecolors='none', c=all_degs, cmap='gist_rainbow')
+    cmap = mpl.colors.ListedColormap(['#e83515','#ff71ce','#fffb96','#48d1cc','#05ffa1','#f18200','#cdbaa2'])
+    # cmap = plt.cm.gist_rainbow
+    bounds = range(1, max_deg+1)
+    norm = mpl.colors.BoundaryNorm(bounds, cmap.N)
+    plt.scatter(x_roots, y_roots, s=list(map(lambda deg: 0.01*(max_deg - deg)**2, all_degs)), marker='.', edgecolors='none', c=all_degs, cmap=cmap, norm=norm)
     ax = plt.gca()
-    ax.set_axis_bgcolor('grey')
-    plt.colorbar()
+    ax.set_axis_bgcolor('black')
+    # ax.set_axis_bgcolor('#ff8c00')
+    plt.colorbar(cmap=cmap, norm=norm, spacing='proportional', ticks=bounds, boundaries=bounds, format='%1i')
     plt.title(str(coefs))
     plt.axis('equal')
-    plt.xlim(-2,2)
-    plt.ylim(-2,2)
+    plt.xlim(-1.5,1.5)
+    plt.ylim(-1.5,1.5)
     return fig
 
 i = 0
 for coefs in coefs_lol:
     fig = make_plot(coefs)
     print('saving fig %i...' % i)
-    plt.savefig('roots_rd_hr_%i.png' % i, figure=fig, dpi=2000)
+    plt.savefig('roots_rd_hr7_%i.png' % i, figure=fig, dpi=2000, bbox_inches='tight')
     i += 1
