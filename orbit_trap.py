7f36566d19454c716e8b432110c46fe921172b39
Neth <kglarrieu@ucdavis.edu>
2019-09-12 01:31:47 -0700

    added julia set

--- orbit_trap.py
+++ orbit_trap.py
@@ -1,7 +1,8 @@
 import numpy as np
 from PIL import Image
 
-def mandelbrot(z0, trap_size, iters=10, deg=7):
+
+def mandelbrot(z0, trap_size, iters=100, deg=3, **kwargs):
     """
     Iterates Mandelbrot set recursive function
     :param z0: initial complex number
@@ -22,6 +23,34 @@ def mandelbrot(z0, trap_size, iters=10, deg=7):
         if abs(z.real) <= trap_size[0] and abs(z.imag) <= trap_size[1]:
             z_out = z
         i += 1
+        if np.absolute(z) > 1e3:  # safe to assume diverged at this point for most IFSs
+            break
+
+    return z_out
+
+def julia(z0, trap_size, iters=100, deg=2, c=0.618):
+    """
+    Iterates Mandelbrot set recursive function
+    :param z0: initial complex number
+    :param iters: number of iterations
+    :param trap_size: 1/2 side lengths of rectangle in complex plane centered at origin for orbit trap
+
+    :return: final complex number within trap
+    """
+    # iteration number
+    i = 1
+    # initial z
+    z = z0
+    # initialize output z
+    z_out = z0
+
+    while i < iters:
+        z = z**deg + c
+        if abs(z.real) <= trap_size[0] and abs(z.imag) <= trap_size[1]:
+            z_out = z
+        i += 1
+        if np.absolute(z) > 1e3:  # safe to assume diverged at this point for most IFSs
+            break
 
     return z_out
 
@@ -32,21 +61,22 @@ def closest(arr, val):
 """
 
 
-def apply_orbit_trap(iter_func, img_path, trap_size=(2,2)):
+def apply_orbit_trap(iter_func, img_path, trap_size=(2, 2)):
     print('reading image...')
     img = Image.open(img_path)
+    # img = img.resize((4096, 4096))
     width, height = img.size
 
     # left to right, top to bottom
     # real and imaginary components in complex plane of initial points (corresponding to pixel centers)
     z_reals = np.linspace(-trap_size[0], trap_size[0], width)
-    z_imags = np.linspace(trap_size[1], -trap_size[1], height)
+    z_imags = np.linspace(-trap_size[1], trap_size[1], height)
 
     # complex number inputs to iterate
     z_ins = [z_r + z_i*1j for z_r in z_reals for z_i in z_imags]
 
     # step sizes in x and y directions in complex plane between pixel centers
-    x_step = z_reals[1] - z_reals[0]
+    x_step = abs(z_reals[1] - z_reals[0])
     y_step = abs(z_imags[1] - z_imags[0])
 
     print('applying iteration...')
@@ -54,10 +84,6 @@ def apply_orbit_trap(iter_func, img_path, trap_size=(2,2)):
 
     print('matching grid cells...')
     # for each z_out, find closest location in pixel grid
-    """
-    z_outs = list(zip([closest(z_reals, z_out.real) for z_out in z_outs],
-                      [closest(z_imags, z_out.imag) for z_out in z_outs]))
-    """
     z_outs = list(zip([x_step * round((z_out.real - trap_size[0])/x_step) + trap_size[0] for z_out in z_outs],
                       [y_step * round((z_out.imag - trap_size[1]) / y_step) + trap_size[1] for z_out in z_outs]))
 
@@ -81,10 +107,10 @@ def apply_orbit_trap(iter_func, img_path, trap_size=(2,2)):
             out_pix[i, j] = tuple(pix_0s[i, j])
 
     print('saving image...')
-    out_img.save("mandelbrot_test2.png")
+    out_img.save("mandelbrot_test12.png")
     print('done.')
 
 
-img_path = 'input.jpg'
-apply_orbit_trap(mandelbrot, img_path)
+img_path = 'input4.jpg'
+apply_orbit_trap(mandelbrot, img_path, trap_size=(2, 3))
 
