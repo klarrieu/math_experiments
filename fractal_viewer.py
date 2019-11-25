cb930e8d0eb00f6c58bcaa1f766bc12a2185a27d
Neth <kglarrieu@ucdavis.edu>
2019-11-25 01:02:08 -0800

    expanding canvas

--- fractal_viewer.py
+++ fractal_viewer.py
@@ -18,44 +18,46 @@ class FractalViewer(object):
         self.fig = Figure()
         self.fig1 = self.fig.add_subplot(111)
         self.canvas = FigureCanvasTkAgg(self.fig, master=self.root)
-        self.canvas.get_tk_widget().grid(sticky=tk.NW, row=0, column=0, columnspan=12)
+        self.canvas.get_tk_widget().pack(fill=tk.BOTH, expand=1)
 
+        self.control_frame = tk.Frame(self.root)
+        self.control_frame.pack()
         # select fractal type
-        self.l_frac_type = tk.Label(self.root, text='Fractal type:')
+        self.l_frac_type = tk.Label(self.control_frame, text='Fractal type:')
         self.l_frac_type.grid(sticky=tk.W, row=1, column=0)
-        self.c_frac_type = ttk.Combobox(self.root, values=['Mandelbrot'], state='readonly')
+        self.c_frac_type = ttk.Combobox(self.control_frame, values=['Mandelbrot'], state='readonly')
         self.c_frac_type.current(0)
         self.c_frac_type.grid(sticky=tk.W, row=1, column=1)
 
         # select colormap
-        self.l_cmap = tk.Label(self.root, text='color map:')
+        self.l_cmap = tk.Label(self.control_frame, text='color map:')
         self.l_frac_type.grid(sticky=tk.W, row=2, column=0)
-        self.c_cmap = ttk.Combobox(self.root, values=self.cmaps, state='readonly')
+        self.c_cmap = ttk.Combobox(self.control_frame, values=self.cmaps, state='readonly')
         self.c_cmap.current(self.cmaps.index('terrain'))
         self.c_cmap.grid(sticky=tk.W, row=2, column=1)
         self.c_cmap.bind('<<ComboboxSelected>>', self.update_cmap)
 
         # select orbit trap
-        self.l_otrap = tk.Label(self.root, text='orbit trap:')
+        self.l_otrap = tk.Label(self.control_frame, text='orbit trap:')
         self.l_otrap.grid(sticky=tk.W, row=3, column=0)
-        self.c_otrap = ttk.Combobox(self.root, values=['magnitude'], state='readonly')
+        self.c_otrap = ttk.Combobox(self.control_frame, values=['magnitude'], state='readonly')
         self.c_otrap.current(0)
         self.c_otrap.grid(sticky=tk.W, row=3, column=1)
 
         # zoom level
-        self.l_zoom = tk.Label(self.root, text='zoom:')
+        self.l_zoom = tk.Label(self.control_frame, text='zoom:')
         self.l_zoom.grid(sticky=tk.W, row=4, column=0)
-        self.s_zoom = tk.Spinbox(self.root, from_=1, to=1e9, command=self.update_zoom)
+        self.s_zoom = tk.Spinbox(self.control_frame, from_=1, to=1e9, command=self.update_zoom)
         self.s_zoom.grid(sticky=tk.W, row=4, column=1)
 
         # navigation buttons
-        self.b_up = tk.Button(self.root, text='↑', command=self.update_up)
+        self.b_up = tk.Button(self.control_frame, text='↑', command=self.update_up)
         self.b_up.grid(row=5, column=5)
-        self.b_down = tk.Button(self.root, text='↓', command=self.update_down)
+        self.b_down = tk.Button(self.control_frame, text='↓', command=self.update_down)
         self.b_down.grid(row=7, column=5)
-        self.b_left = tk.Button(self.root, text='←', command=self.update_left)
+        self.b_left = tk.Button(self.control_frame, text='←', command=self.update_left)
         self.b_left.grid(row=6, column=4)
-        self.b_right = tk.Button(self.root, text='→', command=self.update_right)
+        self.b_right = tk.Button(self.control_frame, text='→', command=self.update_right)
         self.b_right.grid(row=6, column=6)
 
         # initialize image
